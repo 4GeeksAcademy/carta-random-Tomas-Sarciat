66f28932dfb5b0b5d70417bba581cd211fc59a0c
waloleitor <tsarciatroch@gmail.com>
2025-05-07 19:15:01 +0000

    pokerwars

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-import "bootstrap";
-import "./style.css";
+const cardElement = document.getElementById("card");
 
+const suits = [
+  { class: "spade", symbol: "♠" },
+  { class: "club", symbol: "♣" },
+  { class: "heart", symbol: "♥" },
+  { class: "diamond", symbol: "♦" },
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function generateCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  cardElement.className = `card ${randomSuit.class}`;
+  cardElement.innerHTML = `
+    <div class="card-content">
+      <div class="top-left">${randomValue}${randomSuit.symbol}</div>
+      <div style="font-size: 3em;">${randomValue}</div>
+      <div class="bottom-right">${randomValue}${randomSuit.symbol}</div>
+    </div>
+  `;
+}
+
+function updateSize() {
+  const width = document.getElementById("widthInput").value;
+  const height = document.getElementById("heightInput").value;
+  cardElement.style.width = `${width}px`;
+  cardElement.style.height = `${height}px`;
+}
+
+window.onload = () => {
+  generateCard();
+  updateSize();
+  setInterval(generateCard, 5000);
 };
